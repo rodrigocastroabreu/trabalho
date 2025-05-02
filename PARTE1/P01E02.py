994af499584760207e447d3566924f658e991b13
rodrigocastroabreu <rodrigocastrofametro@gmail.com>
2025-05-02 17:45:10 -0300

    P01E02

---
+++ PARTE1/P01E02.py
@@ -0,0 +1,8 @@
+# 2. Faça um Programa que peça dois números e imprima a soma. 
+
+Var_Numero1 = int(input("Digite primeiro numero: "));
+Var_Numero2 = int(input("Digite segundo numero: "));
+
+Var_Resultado = Var_Numero1 + Var_Numero2;
+
+print("O resultado da soma é: ", Var_Resultado);
