79de84f0bd8868d8770f55ede841af7eaf5e9d62
rodrigocastroabreu <rodrigocastrofametro@gmail.com>
2025-05-02 17:58:44 -0300

    P01E05

---
+++ PARTE1/P01E05.py
@@ -0,0 +1,10 @@
+# Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
+var_raio = float(input("Digite valor raio: \n"));
+
+var_calculo = (3.14 * (var_raio ** 2));
+
+print("A área é: ", var_calculo);
+
+
+
