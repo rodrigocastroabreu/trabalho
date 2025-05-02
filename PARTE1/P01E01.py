b2f91cdb38d1057cc7ef0bc1290960ca07982eae
rodrigocastroabreu <rodrigocastrofametro@gmail.com>
2025-05-02 17:43:41 -0300

    P01E01

---
+++ PARTE1/P01E01.py
@@ -0,0 +1,5 @@
+# 1. Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+
+Var_Numero = int(input("Digite um numero: "));
+
+print("O número informado foi",Var_Numero);
