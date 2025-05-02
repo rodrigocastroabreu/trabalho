dc70a15dfe611864bc0744337e5131cf03a70228
rodrigocastroabreu <rodrigocastrofametro@gmail.com>
2025-05-02 17:47:32 -0300

    P01E04

---
+++ PARTE1/P01E04.py
@@ -0,0 +1,8 @@
+# 4. Faça um Programa que converta metros para centímetros.
+
+Var_Metros = float(input("insira valor em metros para converter de metros para cm: "));
+
+Var_Centimentros = Var_Metros *  100;
+
+print("Valor Convertido de metros para centímetros é: ", Var_Centimentros);
+
