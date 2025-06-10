476e3f5e0b2685f989da77b99845fa3bc515a6e8
rodrigocastroabreu <rodrigocastrofametro@gmail.com>
2025-06-10 00:23:19 -0300

    P01E08

--- PARTE1/P01E08.py
+++ PARTE1/P01E08.py
@@ -1,4 +1,4 @@
-#Faça um Programa que peça a temperatura em graus Fahrenheit,
+#8) Faça um Programa que peça a temperatura em graus Fahrenheit,
 # transforme e mostre a temperatura em
 #graus Celsius. C = 5 * ((F-32) / 9).
 
