5bfd80ee88848772cfab5a4190804b767684c8b5
rodrigocastroabreu <rodrigocastrofametro@gmail.com>
2025-06-09 23:48:24 -0300

    P01E09

---
+++ PARTE1/P01E09.py
@@ -0,0 +1,8 @@
+# 9) Faça um Programa que peça a temperatura em graus Celsius,
+# transforme e mostre em graus Fahrenheit.
+
+var_temperatura_Celsius = float(input("Qual temperatura em graus Celsius?\n"));
+
+var_temperatura_Fahrenheit = (var_temperatura_Celsius * 9/5) + 32;
+
+print("A temperatura em graus Fahrenheit é:\n",var_temperatura_Fahrenheit);
