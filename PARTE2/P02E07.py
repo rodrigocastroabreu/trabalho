4993f1a8bfc9b5ddc82647987607cc8384e1fd55
rodrigocastroabreu <rodrigocastrofametro@gmail.com>
2025-06-10 13:45:03 -0300

    P02E07

--- PARTE2/P02E07.py
+++ PARTE2/P02E07.py
@@ -1,8 +1,8 @@
 #7) Faça um Programa que leia três números e mostre o maior deles.
 
-var_numero1 = float(input("Digite o primeiro número:\n"));
-var_numero2 = float(input("Digite o segundo número:\n"));
-var_numero3 = float(input("Digite o terceiro número:\n"));
+var_numero1 = float(input("Digite o primeiro número:\n\t"));
+var_numero2 = float(input("Digite o segundo número:\n\t"));
+var_numero3 = float(input("Digite o terceiro número:\n\t"));
 
 if var_numero1 > var_numero2 and var_numero1 > var_numero3:
     print("O maior número é:\n\t", var_numero1);
@@ -11,4 +11,4 @@ elif var_numero2 > var_numero1 and var_numero2 > var_numero3:
 elif var_numero3 > var_numero1 and var_numero3 > var_numero2:
     print("O maior número é:\n\t", var_numero3);
 else:
-    print("Os números são iguais ou tem valores repetidos.\n");
+    print("Os números são iguais ou tem valores repetidos\n\t");
