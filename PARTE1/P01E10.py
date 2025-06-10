b834d7376ebb2cf84b7bcc90bab0efba6c365755
rodrigocastroabreu <rodrigocastrofametro@gmail.com>
2025-06-10 00:20:25 -0300

    P01E10

--- PARTE1/P01E10.py
+++ PARTE1/P01E10.py
@@ -1,4 +1,4 @@
-#Faça um Programa que peça 2 números inteiros e um número real. Calcule e mostre:
+#10) Faça um Programa que peça 2 números inteiros e um número real. Calcule e mostre:
 #a) o produto do dobro do primeiro com metade do segundo .
 #b) a soma do triplo do primeiro com o terceiro.
 #c) o terceiro elevado ao cubo.
