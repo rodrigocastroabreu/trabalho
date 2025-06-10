a18606c7403f37425c9e4456823b4610f62a9156
rodrigocastroabreu <rodrigocastrofametro@gmail.com>
2025-06-09 23:34:20 -0300

    P01E07

---
+++ PARTE1/P01E07.py
@@ -0,0 +1,10 @@
+# 7) Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês.
+# Calcule e mostre o total do seu salário no referido mês.
+
+var_quanto_ganha_hora = float(input("Quanto ganhar por hora?\n "));
+
+var_numero_horas_trabalhadas = float(input("Número de horas trabalhadas?"));
+
+var_salario_total = var_quanto_ganha_hora * var_numero_horas_trabalhadas;
+
+print("Salario total:\n ",var_salario_total);
