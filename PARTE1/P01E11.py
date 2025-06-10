b2b08d2b1429c49b6d5f780f9c54c44b46c87426
rodrigocastroabreu <rodrigocastrofametro@gmail.com>
2025-06-10 00:31:07 -0300

    P01E11

--- PARTE1/P01E11.py
+++ PARTE1/P01E11.py
@@ -1,8 +1,9 @@
 #11) Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo
 # que calcule seu peso ideal, usando a seguinte fórmula: (72.7*altura) - 58
 
-var_altura = float(input("Qual sua altura em metros?\n"));
+var_altura = float(input("Qual sua altura?\n"));
 
 var_peso_formula = ((72.7 * var_altura) - 58);
 
 print("O peso ideal usando a formula:  (72.7*altura) - 58 é:\n",var_peso_formula);
+
