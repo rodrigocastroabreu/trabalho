b15d93fa78fd0a0ad15a0726aee2f45a1e8eb553
rodrigocastroabreu <rodrigocastrofametro@gmail.com>
2025-06-10 14:58:59 -0300

    P03E06

---
+++ PARTE3/P03E06.py
@@ -0,0 +1,8 @@
+#6) Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. Depois modifique o
+#programa para que ele mostre os números um ao lado do outro.
+
+for var_numero in range(1, 21):
+    print("\n\t",var_numero);
+
+for var_numero in range(1, 21):
+    print(var_numero, end=" ");
